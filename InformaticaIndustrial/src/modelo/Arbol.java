aa4296e3113d62b91dd652d334044b89b81037e9
stefanoantonel <stefanoantonel@gmail.com>
2013-08-16 00:00:37 -0300

    //4 en M

--- InformaticaIndustrial/src/modelo/Arbol.java
+++ InformaticaIndustrial/src/modelo/Arbol.java
@@ -20,7 +20,7 @@ public class Arbol {
 	Nodo buscaNodo= null; //exclusivo para la funcion busar nodo porla recursiviad que me perdia el contexto 
 	ArrayList<Nodo> padresPrincipales = new ArrayList<>();
 	
-	//1
+	//4
 	public Arbol(){
 
 //Obtener descripcion de Articulos
