dc704042fd3843effa67ac11bd822c81d98dca07
stefanoantonel <stefanoantonel@users.noreply.github.com>
2013-08-16 15:38:14 -0300

    j

--- InformaticaIndustrial/src/ui/ExplosionUI.java
+++ InformaticaIndustrial/src/ui/ExplosionUI.java
@@ -30,7 +30,7 @@ public class ExplosionUI extends JFrame {
 
 	public ExplosionUI(DefaultTreeModel modelo) {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 568, 371);
+		setBounds(100, 100, 568, 371);//jhkj
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
