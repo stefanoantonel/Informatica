c9ca78ca5350b2a1aedcb6d426abf44dd045957a
stefanoantonel <stefanoantonel@gmail.com>
2013-09-06 21:33:31 -0300

    txt

--- src/txt/Lectura.java
+++ src/txt/Lectura.java
@@ -1,29 +1,37 @@
 package txt;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
 
 import javax.swing.JOptionPane;
+
 
 public class Lectura {
 
     public static void main(String[] args) {
+    	StringBuilder retornar=new StringBuilder(); //va acumulando todo
+        String linea;	//contiene la linea
+        File archivo=new File("C:\\leer.txt");
+        FileReader fileReader=null; //abre el archivo en disco
+        BufferedReader br=null; //buffer de todo el archivo
+        int contadorLinea=0;
+        
         try{
-        String retornar="";
-        String linea="";
-        
-        
-        BufferedReader br=new BufferedReader(new FileReader("leer.txt"));
-        br.read();
-        while ((linea = br.readLine()) != null){
-            retornar += linea + "\n";
-        }
+        	fileReader=new FileReader(archivo);
+        	br=new BufferedReader(fileReader);
+	        while ((linea = br.readLine()) != null){
+	        	System.out.println(linea);
+	            retornar.append(linea);
+	            retornar.append("\n");
+	        }
+	    fileReader.close();
         JOptionPane.showMessageDialog(null, retornar);
-        br.close();
         }
         catch ( IOException e){
-            JOptionPane.showMessageDialog(null, "ERROR \nCree un archivo 'leer.txt' en la carpeta donde ejecuta el archivo");
+            JOptionPane.showMessageDialog(null, "ERROR \nCree un archivo 'leer.txt' en C: ");
         }
     }
 }
