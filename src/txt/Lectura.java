518d16747b9418420d6f7d283eb5a7b4a5778157
stefanoantonel <stefanoantonel@gmail.com>
2013-09-08 21:51:11 -0300

    separe cabecera y pie

--- src/txt/Lectura.java
+++ src/txt/Lectura.java
@@ -12,8 +12,11 @@ import javax.swing.JOptionPane;
 public class Lectura {
 
     public static void main(String[] args) {
+    	
     	StringBuilder retornar=new StringBuilder(); //va acumulando todo
         String linea;	//contiene la linea
+        String cabecera;
+        String pie = null;
         File archivo=new File("C:\\leer.txt");
         FileReader fileReader=null; //abre el archivo en disco
         BufferedReader br=null; //buffer de todo el archivo
@@ -22,16 +25,25 @@ public class Lectura {
         try{
         	fileReader=new FileReader(archivo);
         	br=new BufferedReader(fileReader);
+        	linea=br.readLine();
+        	leerCabecera(linea);
 	        while ((linea = br.readLine()) != null){
 	        	System.out.println(linea);
 	            retornar.append(linea);
 	            retornar.append("\n");
+	            pie=linea;
 	        }
+	        leerPie(pie);
 	    fileReader.close();
         JOptionPane.showMessageDialog(null, retornar);
         }
-        catch ( IOException e){
-            JOptionPane.showMessageDialog(null, "ERROR \nCree un archivo 'leer.txt' en C: ");
-        }
+        catch ( IOException e){JOptionPane.showMessageDialog(null, "ERROR \nCree un archivo 'leer.txt' en C: ");}
+    }
+    
+    private static void leerCabecera(String cabecera){
+    	System.out.println("cabecera: "+cabecera);
+    }
+    private static void leerPie(String pie){
+    	System.out.println("pie: "+pie);
     }
 }
