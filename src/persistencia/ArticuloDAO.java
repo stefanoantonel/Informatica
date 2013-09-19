17722b9417cfc2b7b6920e54d97a7986a13a0039
stefanoantonel <stefanoantonel@gmail.com>
2013-09-19 13:38:18 -0300

    agregue articuloDAO

---
+++ src/persistencia/ArticuloDAO.java
@@ -0,0 +1,34 @@
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ArticuloDAO {
+
+	public Integer obetenerCP(String id) {
+		Conexion cn1 = new Conexion();
+		Connection con = cn1.getConexion();
+		ResultSet rs = null;
+		Integer cp = -1;
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("select codigo_plano from Articulo where id=");
+			sb.append(id);
+
+			PreparedStatement stm;
+			stm = con.prepareStatement(sb.toString());
+			// stm.setString(1, alm);
+			rs = stm.executeQuery();
+
+			while (rs.next()) {
+				cp = rs.getInt("codigo_plano");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("error en obtenerCP");
+		}
+		return cp;
+	}
+}
